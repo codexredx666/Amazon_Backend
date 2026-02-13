79f30199b9df95838ac827246fd54182538fb354
sukuna-666 <sukhisamu564@gmail.com>
2026-02-13 15:55:29 +0530

    commit

--- db.py
+++ db.py
@@ -1,17 +1,21 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+Base=declarative_base() 
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+DATABASE_URL=os.getenv("DATABASE_URL")
+print("DATABASE_URL",DATABASE_URL)
+#create engine once at module level
+engine=create_engine(DATABASE_URL)
 
+#create sessionaLocal class for creatimg database sessions
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 def get_db():
-    db = SessionLocal()
+    db=SessionLocal()
     try:
         yield db
     finally:
--- models.py
+++ models.py
@@ -1,3 +1,12 @@
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from db import Base
+from sqlalchemy import Column, Integer, String, Boolean
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+    address = Column(String) 
+    phone = Column(String)
+    isprime= Column(Boolean)
+    
